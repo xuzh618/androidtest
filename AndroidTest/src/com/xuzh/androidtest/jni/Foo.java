c35d1a0b9c8c2c5d89c1ec6803e7644407646bf2
jack shy <xuzh06nenu@126.com>
2014-07-23 16:48:47 +0800

    使用javah 命令生成对应的native C代码 javah 到.class目录下，android工程都在bin/classes下 执行命令：javah -d ../../jni -jni com.xuzh.androidtest.jni.Foo 就会发现jni目录下有 生成对用的com_xuzh_androidtest_jni_Foo.h文件

---
+++ AndroidTest/src/com/xuzh/androidtest/jni/Foo.java
@@ -0,0 +1,16 @@
+package com.xuzh.androidtest.jni;
+
+public class Foo {
+
+    /**
+     * 使用javah 命令生成对应的native C代码
+     * 
+     * javah 到.class目录下，android工程都在bin/classes下
+     * 执行命令：javah -d ../../jni -jni com.xuzh.androidtest.jni.Foo
+     * 就会发现jni目录下有 生成对用的com_xuzh_androidtest_jni_Foo.h文件
+     * 
+     */
+    native void method1();
+
+    native void method2();
+}
